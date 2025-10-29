5cf68a8415a4aa0f97ec719a2cc8e27bacd7276c
rc-earnest <rc.earnest04@gmail.com>
2025-10-29 16:26:33 -0600

    Working code

---
+++ Task2/Task2.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Task2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        Me.ComComboBox = New System.Windows.Forms.ComboBox()
+        Me.RefreshButton = New System.Windows.Forms.Button()
+        Me.DisconnectButton = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.SendDollarButton = New System.Windows.Forms.Button()
+        Me.ServoDataButton = New System.Windows.Forms.Button()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(544, 416)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 3
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(713, 415)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 16)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(776, 228)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(12, 415)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(75, 23)
+        Me.ConnectButton.TabIndex = 0
+        Me.ConnectButton.Text = "Connect"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'ComComboBox
+        '
+        Me.ComComboBox.FormattingEnabled = True
+        Me.ComComboBox.Location = New System.Drawing.Point(93, 415)
+        Me.ComComboBox.Name = "ComComboBox"
+        Me.ComComboBox.Size = New System.Drawing.Size(121, 24)
+        Me.ComComboBox.TabIndex = 1
+        '
+        'RefreshButton
+        '
+        Me.RefreshButton.Location = New System.Drawing.Point(220, 416)
+        Me.RefreshButton.Name = "RefreshButton"
+        Me.RefreshButton.Size = New System.Drawing.Size(75, 23)
+        Me.RefreshButton.TabIndex = 2
+        Me.RefreshButton.Text = "Refresh"
+        Me.RefreshButton.UseVisualStyleBackColor = True
+        '
+        'DisconnectButton
+        '
+        Me.DisconnectButton.Location = New System.Drawing.Point(381, 415)
+        Me.DisconnectButton.Name = "DisconnectButton"
+        Me.DisconnectButton.Size = New System.Drawing.Size(111, 23)
+        Me.DisconnectButton.TabIndex = 5
+        Me.DisconnectButton.Text = "Disconnect"
+        Me.DisconnectButton.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(150, 292)
+        Me.TrackBar1.Maximum = 25
+        Me.TrackBar1.Minimum = 5
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(430, 56)
+        Me.TrackBar1.TabIndex = 6
+        Me.TrackBar1.Value = 5
+        '
+        'Timer1
+        '
+        '
+        'StopButton
+        '
+        Me.StopButton.Location = New System.Drawing.Point(625, 415)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(75, 23)
+        Me.StopButton.TabIndex = 7
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'SendDollarButton
+        '
+        Me.SendDollarButton.Location = New System.Drawing.Point(12, 386)
+        Me.SendDollarButton.Name = "SendDollarButton"
+        Me.SendDollarButton.Size = New System.Drawing.Size(202, 23)
+        Me.SendDollarButton.TabIndex = 8
+        Me.SendDollarButton.Text = "Send Dollar"
+        Me.SendDollarButton.UseVisualStyleBackColor = True
+        '
+        'ServoDataButton
+        '
+        Me.ServoDataButton.Location = New System.Drawing.Point(12, 357)
+        Me.ServoDataButton.Name = "ServoDataButton"
+        Me.ServoDataButton.Size = New System.Drawing.Size(202, 23)
+        Me.ServoDataButton.TabIndex = 9
+        Me.ServoDataButton.Text = "Send ! and Get Servo Data"
+        Me.ServoDataButton.UseVisualStyleBackColor = True
+        '
+        'Task2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ServoDataButton)
+        Me.Controls.Add(Me.SendDollarButton)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.DisconnectButton)
+        Me.Controls.Add(Me.RefreshButton)
+        Me.Controls.Add(Me.ComComboBox)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.Name = "Task2"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StartButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ConnectButton As Button
+    Friend WithEvents ComComboBox As ComboBox
+    Friend WithEvents RefreshButton As Button
+    Friend WithEvents DisconnectButton As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StopButton As Button
+    Friend WithEvents SendDollarButton As Button
+    Friend WithEvents ServoDataButton As Button
+End Class
---
+++ Task2/Task2.vb
@@ -0,0 +1,297 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
+Imports System.ComponentModel
+Imports System.IO.Ports
+Public Class Task2
+    Public dIn As Byte
+    Public timerCount As Integer
+    Sub Write1()
+        Dim read0(SerialPort1.BytesToRead) As Byte
+        Dim read1(SerialPort1.BytesToRead) As Byte
+        Dim data0(0) As Byte
+        Dim data1(0) As Byte
+        data0(0) = &B100100 ' ASCII for $
+        SerialPort1.Write(data0, 0, 1)
+        SerialPort1.Read(read0, 0, SerialPort1.BytesToRead)
+        If read0(0) = &B100100 Then
+            Select Case TrackBar1.Value
+                Case 5
+                    data1(0) = &H1
+                Case 6
+                    data1(0) = &H2
+                Case 7
+                    data1(0) = &H3
+                Case 8
+                    data1(0) = &H4
+                Case 9
+                    data1(0) = &H5
+                Case 10
+                    data1(0) = &H6
+                Case 11
+                    data1(0) = &H7
+                Case 12
+                    data1(0) = &H8
+                Case 13
+                    data1(0) = &H9
+                Case 14
+                    data1(0) = &HA
+                Case 15
+                    data1(0) = &HB
+                Case 16
+                    data1(0) = &HC
+                Case 17
+                    data1(0) = &HD
+                Case 18
+                    data1(0) = &HE
+                Case 19
+                    data1(0) = &HF
+                Case 20
+                    data1(0) = &H10
+                Case 21
+                    data1(0) = &H11
+                Case 22
+                    data1(0) = &H12
+                Case 23
+                    data1(0) = &H13
+                Case 24
+                    data1(0) = &H14
+                Case 25
+                    data1(0) = &H15
+            End Select
+            SerialPort1.Write(data1, 0, 1)
+        End If
+    End Sub
+
+    Sub Write2()
+        Dim read0(SerialPort1.BytesToRead) As Byte
+        Dim read1(SerialPort1.BytesToRead) As Byte
+        Dim data0(0) As Byte
+        Dim data1(0) As Byte
+        Dim mask7 As Byte = 128
+        Dim mask6 As Byte = 64
+        Dim mask5 As Byte = 32
+        Dim mask4 As Byte = 16
+        Dim mask3 As Byte = 8
+        Dim totalWeight As Integer = 0
+        Dim weightedByte(0) As Byte
+        data0(0) = &B100100 ' ASCII for $
+        SerialPort1.Write(data0, 0, 1)
+        SerialPort1.Read(read0, 0, SerialPort1.BytesToRead)
+        If read0(0) = &B100100 Then
+            SerialPort1.Read(read1, 0, SerialPort1.BytesToRead)
+            Dim _read As Byte = read1(0)
+            If (_read And mask7) <> 0 Then
+                totalWeight += 16
+            End If
+            If (_read And mask6) <> 0 Then
+                totalWeight += 8
+            End If
+            If (_read And mask5) <> 0 Then
+                totalWeight += 4
+            End If
+            If (_read And mask4) <> 0 Then
+                totalWeight += 2
+            End If
+            If (_read And mask3) <> 0 Then
+                totalWeight += 1
+            End If
+            weightedByte(0) = CByte(totalWeight)
+            SerialPort1.Write(weightedByte, 0, 1)
+        End If
+    End Sub
+
+    Function WriteDollar() As Byte
+        Dim dollar(0) As Byte
+        dollar(0) = &B100100
+        SerialPort1.Write(dollar, 0, 1)
+        Dim read0(SerialPort1.BytesToRead) As Byte
+        SerialPort1.Read(read0, 0, SerialPort1.BytesToRead)
+        Return read0(0)
+    End Function
+    Enum Flags
+        Bit7 = CInt(2 ^ 7)
+        Bit6 = CInt(2 ^ 6)
+        Bit5 = CInt(2 ^ 5)
+        Bit4 = CInt(2 ^ 4)
+        Bit3 = CInt(2 ^ 3)
+    End Enum
+
+    Sub WriteandRead()
+        Dim lessThan(0) As Byte
+        Dim servoData(0) As Byte
+        Dim container As Integer
+        Dim temp As Integer
+        Dim dollar(0) As Byte
+        dollar(0) = &B100100
+        SerialPort1.DiscardInBuffer()
+        SerialPort1.Write(dollar, 0, 1)
+        Dim read0 As Integer = SerialPort1.ReadByte()
+        If read0 = &B100100 Then
+
+            lessThan(0) = &H3C
+            SerialPort1.DiscardInBuffer()
+            SerialPort1.Write(lessThan, 0, 1)
+            SerialPort1.ReadByte()
+            container = SerialPort1.ReadByte
+            If (container And Flags.Bit7) = Flags.Bit7 Then
+                temp += 16
+            End If
+            If (container And Flags.Bit6) = Flags.Bit6 Then
+                temp += 8
+            End If
+            If (container And Flags.Bit5) = Flags.Bit5 Then
+                temp += 4
+            End If
+            If (container And Flags.Bit4) = Flags.Bit4 Then
+                temp += 2
+            End If
+            If (container And Flags.Bit3) = Flags.Bit3 Then
+                temp += 1
+            End If
+
+            servoData(0) = CType(temp, Byte)
+            SerialPort1.Write(servoData, 0, 1)
+        End If
+    End Sub
+
+    Private Sub PopulateComPorts()
+        ComComboBox.Items.Clear()
+
+        ' Get an array of all available COM port names on the system
+        Dim portNames As String() = SerialPort.GetPortNames()
+
+        If portNames.Length > 0 Then
+            ' Add each port name to the ComboBox
+            For Each portName In portNames
+                ComComboBox.Items.Add(portName)
+            Next
+
+            ' Select the first item in the list by default
+            ComComboBox.SelectedIndex = 0
+        Else
+            ' Handle case where no ports are found
+            ComComboBox.Items.Add("No COM Ports Found")
+            ComComboBox.SelectedIndex = 0
+            ConnectButton.Enabled = False ' Disable connect button
+        End If
+    End Sub
+
+
+    Sub Connect()
+        Try
+            ' 1. Set the port properties based on ComboBox selection
+            SerialPort1.PortName = ComComboBox.SelectedItem.ToString()
+
+            ' 2. Set Baud Rate 
+            SerialPort1.BaudRate = 9600
+
+            ' 3. Set standard data format properties
+            SerialPort1.DataBits = 8
+            SerialPort1.Parity = Parity.None
+            SerialPort1.StopBits = StopBits.One
+
+            ' 4. Open the connection
+            SerialPort1.Open()
+
+            ' 5. Update UI state
+            UpdateConnectionState(True)
+            ListBox1.Items.Add($"Successfully connected to {SerialPort1.PortName} at {SerialPort1.BaudRate} Baud.")
+
+        Catch ex As Exception
+            ' Handle errors (e.g., port already in use, invalid settings, permissions)
+            MessageBox.Show($"Could not open port: {ex.Message}", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            UpdateConnectionState(False)
+        End Try
+    End Sub
+
+    Private Sub DisconnectButton_Click(sender As Object, e As EventArgs) Handles DisconnectButton.Click
+        Timer1.Enabled = False
+        If SerialPort1.IsOpen Then
+            Try
+                ' Close the connection
+                SerialPort1.Close()
+
+                ' Update UI state
+                UpdateConnectionState(False)
+                ListBox1.Items.Add("Disconnected successfully.")
+
+            Catch ex As Exception
+                ' Handle errors during close (rare, but good practice)
+                MessageBox.Show($"Error during disconnect: {ex.Message}", "Disconnection Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+
+    ' Helper function to manage button and ComboBox state
+    Private Sub UpdateConnectionState(ByVal isConnected As Boolean)
+        ConnectButton.Enabled = Not isConnected
+        StartButton.Enabled = isConnected
+        DisconnectButton.Enabled = isConnected
+        ComComboBox.Enabled = Not isConnected ' Disable changing port while connected
+    End Sub
+
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Console.WriteLine(SerialPort1.BytesToRead)
+    End Sub
+
+
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+
+
+
+
+        Timer1.Enabled = True
+        'Write()
+    End Sub
+
+    Private Sub Task2_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+        End If
+    End Sub
+
+    Private Sub ConnectButton_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+        Connect()
+    End Sub
+
+    Private Sub RefreshButton_Click(sender As Object, e As EventArgs) Handles RefreshButton.Click
+        PopulateComPorts()
+    End Sub
+
+    Private Sub Task2_Load(sender As Object, e As EventArgs) Handles Me.Load
+        UpdateConnectionState(False)
+        PopulateComPorts()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Write1()
+        ' Write2()
+        Dim tempByte As Byte
+        tempByte = WriteDollar()
+        ' If tempByte = &B100100 Then
+
+        WriteandRead()
+       ' End If
+    End Sub
+
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub SendDollarButton_Click(sender As Object, e As EventArgs) Handles SendDollarButton.Click
+        WriteDollar()
+    End Sub
+
+    Private Sub ServoDataButton_Click(sender As Object, e As EventArgs) Handles ServoDataButton.Click
+        WriteandRead()
+    End Sub
+End Class
